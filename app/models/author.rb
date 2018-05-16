b284e44cd90789d360332d903cf2313c23eaf4df
ronek22 <jronek3010@gmail.com>
2018-05-17 01:11:41 +0200

    Correct alignments to match standards & Remove unnecessary validator

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ApplicationRecord
     has_many :books, dependent: :delete_all
-    validates :firstname, :lastname, :nationality, :length => { :in => 3..20 }, format: { with: /\A[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+\z/, message: " contain(s) numbers or/and first letter is small" }
+    validates :firstname, :lastname,:length => { :in => 3..20 }, format: { with: /\A[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+\z/, message: " contain(s) numbers or/and first letter is small" }
     validates :bio, :length => { :in => 0..300 }
 
     def name
