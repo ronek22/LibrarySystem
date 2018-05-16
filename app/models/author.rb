153bdd2133810ec254787a92f29196f1ecbbbbe3
sakovski <sakasew@gmail.com>
2018-05-16 18:04:43 +0200

    Fixed Books form bug

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ApplicationRecord
     validates :firstname, :lastname, :nationality, :length => { :in => 3..20 }, format: { with: /\A[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+\z/, message: " contain(s) numbers or/and first letter is small" }
     validates :bio, :length => { :in => 0..300 }
+
+    def name
+      "#{lastname} #{firstname}"
+    end
 end
