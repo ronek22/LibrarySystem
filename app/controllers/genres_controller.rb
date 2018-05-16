f954ceece16affaf282381ed6a33c62c7e2e4b3f
sakovski <sakasew@gmail.com>
2018-05-16 14:32:05 +0200

    CRUD for genres with pagination, search form and sroting done

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,10 +1,15 @@
 class GenresController < ApplicationController
   before_action :set_genre, only: [:show, :edit, :update, :destroy]
+  helper_method :sort_column, :sort_direction
 
   # GET /genres
   # GET /genres.json
   def index
-    @genres = Genre.all
+    @tasks = if params[:toSearch]
+      @genres = Genre.search(params[:toSearch]).paginate(:page => params[:page], :per_page => 5)
+    else
+      @genres = Genre.order("#{sort_column} #{sort_direction}").paginate(:page => params[:page], :per_page => 5)
+    end
   end
 
   # GET /genres/1
@@ -70,5 +75,13 @@ class GenresController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def genre_params
       params.require(:genre).permit(:name, :desc)
+    end
+
+    def sort_column
+      params[:sort] || "name"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
     end
 end
