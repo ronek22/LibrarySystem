6bae62bbb7cd7e0a0c6d02f09f15b7a875a4b110
sakovski <sakasew@gmail.com>
2018-05-16 17:56:20 +0200

    Datepicker error fixec

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,16 +1,4 @@
 class Author < ApplicationRecord
     validates :firstname, :lastname, :nationality, :length => { :in => 3..20 }, format: { with: /\A[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+\z/, message: " contain(s) numbers or/and first letter is small" }
     validates :bio, :length => { :in => 0..300 }
-
-    # nie potrzebne przy date
-    validates_each :date_of_birth do |record, attr, value|
-      begin
-        Date.parse(value)
-      rescue
-        record.errors.add(attr, "Invalid date")
-      end
-    end
-    def name
-    "#{lastname} #{firstname}"
-    end
 end
